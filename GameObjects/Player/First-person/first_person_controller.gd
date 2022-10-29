bc91f5009eb77564f3fecccdd3231b5b66992994
warriormaster12 <streng.alexander@outlook.com>
2022-10-29 22:34:19 +0300

    simple object interaction WIP

--- GameObjects/Player/First-person/first_person_controller.gd
+++ GameObjects/Player/First-person/first_person_controller.gd
@@ -7,12 +7,17 @@ extends CharacterBody3D
 @export var mouse_sens:float = 0.3
 
 @onready var head:Node3D = $Head
+@onready var interaction_ray:RayCast3D = $Head/Camera3D/InteractionRay
+
+var interactable_target:RigidBody3D = null
+var toggle_interaction:bool = false
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	interaction_ray.add_exception(self)
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -22,6 +27,30 @@ func _input(event):
 	
 	if event.is_action_pressed("exit_game"):
 		get_tree().quit()
+	if event.is_action_pressed("Interact"):
+		if toggle_interaction:
+			toggle_interaction = false
+		else:
+			if interaction_ray.is_colliding():
+				if interaction_ray.get_collider().is_in_group("interactable"):
+					toggle_interaction = true
+					interactable_target = interaction_ray.get_collider()
+
+func _process(delta):
+	if interactable_target != null:
+		if toggle_interaction:
+			interactable_target.freeze = true
+			interactable_target.get_child(1).disabled = true
+			if interaction_ray.is_colliding() and not interaction_ray.get_collider().is_in_group("interactable"):
+				interactable_target.global_position = interaction_ray.get_collision_point() + Vector3(0, 0.45, 0)
+			else:
+				interaction_ray.target_position.z = -3.0
+				interactable_target.global_position = interaction_ray.to_global(interaction_ray.target_position)
+		else:
+			interaction_ray.target_position.z = -5.0
+			interactable_target.freeze = false
+			interactable_target.get_child(1).disabled = false
+			interactable_target = null
 
 func _physics_process(delta):
 	# Add the gravity.
